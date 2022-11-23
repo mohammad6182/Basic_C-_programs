cbb42bc947d55d3edb9b07009af3783d313c445b
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-11-23 15:42:53 -0800

    add

--- ArraySubmiission/ArraySubmiission/Program.cs
+++ ArraySubmiission/ArraySubmiission/Program.cs
@@ -15,36 +15,44 @@ namespace ArraySubmiission
             Console.WriteLine("Please choose a index");
             int index = Convert.ToInt32(Console.ReadLine());
             //whith using if statemnet, telling the array how to react to the different orders
-            if (index < mineWords.Length)
+            if (index < mineWords.Length && index >= 0)
             {
                 Console.WriteLine(mineWords[index]);
                 Console.ReadLine();
 
             }
+            else
+            {
+                Console.WriteLine("This index is out of bounds");
+            }
 
             int[] mineNumbers = { 1,3,9,16,444 };
             Console.WriteLine("Please choose a index number");
             int index1 = Convert.ToInt32(Console.ReadLine());
-            if (index1 < mineNumbers.Length)
+            if (index1 < mineNumbers.Length && index1 >= 0)
             {
                 Console.WriteLine(mineNumbers[index1]);
                 Console.ReadLine();
             }
-            if (index1 > mineNumbers.Length)
+            else
             {
-                Console.WriteLine("No, That is ot a valid choice, try again");
-                Console.ReadLine();
+                Console.WriteLine("No, That is not a valid choice, try again");
+                
             }
 
             List<string> names = new List<string>() { "John", "Alex", "Mike", "Tom" };
             Console.WriteLine("Choose index to show name");
             int index2 = Convert.ToInt32(Console.ReadLine());
-            if (index2 < names.Count)
+            if (index2 < names.Count && index2<=0)
             {
                 Console.WriteLine(names[index2]);
                 Console.ReadLine();
             }
-
+            else
+            {
+                Console.WriteLine("Not Valid number");
+            }
+            Console.ReadLine();
 
         }
     }
---
+++ page256Assignment/page256Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page256Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part One Assignment 
+            string[] colors = { "Pink", "Blue", "Red", "White" };
+            Console.WriteLine("what color are we missing ?");
+            Console.ReadLine();
+
+
+        }
+    }
+}

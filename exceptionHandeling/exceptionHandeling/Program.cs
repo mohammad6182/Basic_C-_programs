40e75b000f805c5a7c01087162c2c33a8d4523ac
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-11-24 19:18:14 -0800

    add

---
+++ exceptionHandeling/exceptionHandeling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandeling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We can put the wholw code under "TRY" and thiswa y we can add exceptions Later one, Exceptions can be general or more specific
+            try
+            {
+                Console.WriteLine("Pick a number: ");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a asecond number:");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two....");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                // this is an automatic massage 
+                Console.WriteLine(ex.Message);
+
+                // or we can print our own massage 
+                // Console.WriteLine(" Please type a whole number ");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont divide by zero");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ page 263/page 263/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page_263
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+            {
+                // making list to start with
+                List<int> numbers = new List<int>() { 20, 14, 18, 1 };
+                Console.WriteLine("Pick a number to start dividing...");
+                // converting to int to start dividing
+                int d = Convert.ToInt32(Console.ReadLine());
+                for (int n = 0; n < numbers.Count; n++)
+                {
+                    Console.WriteLine(n / d);
+                    Console.ReadLine();
+                }
+                
+                
+            }
+        catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont choose zerto ");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }          
+            
+        }
+    }
+}

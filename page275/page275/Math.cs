7293f19f58fdf135bd5118fc88dfc733d92cf5cc
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-11-25 09:55:52 -0800

    add

---
+++ page275/page275/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page275
+{
+   public class Math
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 2, 3, 5, 7, 8 };
+            Console.WriteLine("Add a number: ");
+            int numbers2 = Convert.ToInt32(Console.ReadLine());
+            foreach (int num in numbers2)
+            {
+                if (num >= 0)
+                {
+                    Console.WriteLine(num + numbers);
+                    
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}

1479a9ecacef964ce6b0c4cf227ab730a5050a49
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-11-28 20:25:25 -0800

    add

---
+++ 281PAGE/281PAGE/Assignmet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page281
+{
+    class Assignment
+    {
+        public void Math(int number1, int number2)
+        {
+            int results = number1 + 6;
+            return results;
+        }
+    }
+}
---
+++ 281PAGE/281PAGE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment x = new Assignment();
+            Console.WriteLine("pick two numbers");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number2);
+            Console.ReadLine();
+        }
+    }
+}

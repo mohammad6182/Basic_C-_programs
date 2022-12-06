90962086b3e86d3a8276019489038dacd6b33066
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-12-05 20:26:06 -0800

    add

---
+++ page302/page302/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page302
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("name : " + this.FirstName + " " + this.LastName) ; 
+        }
+        public void Quit(Employee employee)
+        {
+
+        }
+
+    }
+
+    
+
+    
+}
---
+++ page302/page302/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page302
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+
+            };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}

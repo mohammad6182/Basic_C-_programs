af0a9c8f16d26a80ea70d9eb8222048dc70eb217
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-12-13 21:05:20 -0800

    add

---
+++ Page370/Page370/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Page370
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("pick a number between 1 and 20");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("in {0} hours, the time will be " + DateTime.Now.AddHours(x) , x);
+            Console.ReadLine();
+        }
+    }
+}
